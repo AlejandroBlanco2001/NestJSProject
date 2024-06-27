5473dfb9892a41b374f1e0f888d2cb74f34367f4
Isaac Blanco <nuclearhands2001@gmail.com>
2024-06-27 00:53:45 -0500

    add: More documentation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform, TransformFnParams } from 'class-transformer';
 import {
   IsInt,
@@ -14,19 +15,43 @@ export class CreateProductDto implements Readonly<CreateProductDto> {
   @IsNotEmpty()
   @Transform(({ value }: TransformFnParams) => value?.trim())
   @MaxLength(255)
+  @ApiProperty({
+    description: 'Nombre del producto',
+    required: true,
+    default: 'Producto',
+    type: String,
+  })
   nombre: string;
 
   @IsString()
   @IsOptional()
   @Transform(({ value }: TransformFnParams) => value?.trim())
+  @ApiProperty({
+    description: 'Descripción del producto',
+    required: false,
+    default: 'Descripción del producto',
+    type: String,
+  })
   descripcion?: string;
 
   @IsNumber()
   @IsPositive()
+  @ApiProperty({
+    description: 'Precio del producto',
+    required: true,
+    default: 0,
+    type: Number,
+  })
   precio: number;
 
   @IsNumber()
   @IsPositive()
   @IsInt()
+  @ApiProperty({
+    description: 'Cantidad de productos',
+    required: true,
+    default: 0,
+    type: Number,
+  })
   cantidad: number;
 }
--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,65 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform, TransformFnParams } from 'class-transformer';
+import {
+  IsString,
+  IsNotEmpty,
+  MaxLength,
+  IsOptional,
+  IsNumber,
+  IsPositive,
+  IsInt,
+} from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+/**
+ * I duplicated the code from the create-product.dto.ts
+ * to be able to use APIProperty decorator,
+ * it's not necessary to do this at all. God bless NestJS
+ */
+
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @MaxLength(255)
+  @ApiProperty({
+    description: 'Nombre del producto',
+    required: true,
+    default: 'Producto',
+    type: String,
+  })
+  nombre: string;
+
+  @IsString()
+  @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @ApiProperty({
+    description: 'Descripción del producto',
+    required: false,
+    default: 'Descripción del producto',
+    type: String,
+  })
+  descripcion?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Precio del producto',
+    required: true,
+    default: 0,
+    type: Number,
+  })
+  precio: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsInt()
+  @ApiProperty({
+    description: 'Cantidad de productos',
+    required: true,
+    default: 0,
+    type: Number,
+  })
+  cantidad: number;
+}
