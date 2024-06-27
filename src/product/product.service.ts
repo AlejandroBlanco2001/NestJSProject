d5e27051a237d743294088df407fc8c342c8bae6
Isaac Blanco <nuclearhands2001@gmail.com>
2024-06-27 00:27:29 -0500

    add: more loggers and removing unnecesary entityt product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Repository } from 'typeorm';
-import { Product } from './entities/product.entity';
+import { Product } from 'src/model/product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
@@ -13,12 +13,22 @@ export class ProductService {
     @InjectRepository(Product) private readonly repo: Repository<Product>,
   ) {}
 
-  create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto) {
+    this.logger.log(`Creating product with name ${createProductDto.nombre}`);
+
+    const product = await this.repo.create(createProductDto);
+
+    this.logger.log(`Product created with id ${product.id}`);
+
     return this.repo.save(createProductDto);
   }
 
-  findAll() {
-    return this.repo.find();
+  async findAll() {
+    this.logger.log('Finding all products');
+
+    const products = await this.repo.find();
+
+    return products;
   }
 
   async findOne(id: number) {
