63f784e9f3e85772744f707abb9009d4990e154b
Isaac Blanco <nuclearhands2001@gmail.com>
2024-06-27 00:46:35 -0500

    add: adding Swagger documentation and missing endpoints formatting

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -9,16 +9,24 @@ import {
   ParseIntPipe,
   ValidationPipe,
   UsePipes,
+  HttpCode,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a product' })
+  @ApiResponse({
+    status: 201,
+    description: 'The product has been successfully created',
+  })
   @UsePipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
@@ -31,16 +39,31 @@ export class ProductController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all the products' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all the products',
+  })
   findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a particular product given the id' })
+  @ApiResponse({
+    status: 404,
+    description: 'Product not found',
+  })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.productService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a product' })
+  @ApiResponse({
+    status: 204,
+    description: 'The product has been successfully updated',
+  })
   @UsePipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
@@ -48,15 +71,22 @@ export class ProductController {
       transform: true,
     }),
   )
+  @HttpCode(204)
   update(
-    @Param('id', ParseIntPipe) id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productService.update(+id, updateProductDto);
+    return this.productService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: string) {
-    return this.productService.remove(+id);
+  @ApiOperation({ summary: 'Delete a product' })
+  @ApiResponse({
+    status: 204,
+    description: 'The product has been successfully deleted',
+  })
+  @HttpCode(204)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.remove(id);
   }
 }
