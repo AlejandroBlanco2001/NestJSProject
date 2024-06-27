170ad90a69d89c96df2331314e5695cad055aa4c
Isaac Blanco <nuclearhands2001@gmail.com>
2024-06-27 00:02:27 -0500

    add: created DTOs

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProductDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class CreateProductDto implements Readonly<CreateProductDto> {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsString()
+  @IsOptional()
+  descripcion?: string;
+
+  @IsNumber()
+  @IsPositive()
+  precio: number;
+
+  @IsNumber()
+  @IsPositive()
+  cantidad: number;
+}
